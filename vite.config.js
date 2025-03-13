f7de0169a42b4f1f8305494ad9a44848a06e54ee
franto <radrienfrantos@gmail.com>
2025-03-13 17:09:43 +0100

    algo djisktra 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
+  },
   plugins: [react()],
 })
+
